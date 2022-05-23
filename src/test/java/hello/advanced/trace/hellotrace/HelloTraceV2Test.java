0cdbdc03da907d5caed03c47b870097cef675796
fDevJc <fdevjc@gmail.com>
2022-05-23 12:21:55 +0900

    예제 만들기

---
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,29 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV2Test {
+    @Test
+    void begin_end() {
+
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status = trace.begin("hello");
+        TraceStatus status1 = trace.beginSync(status.getTraceId(), "helooooooo");
+        trace.end(status1);
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status = trace.begin("hello");
+        TraceStatus status1 = trace.beginSync(status.getTraceId(), "helooooooo");
+        trace.exception(status1, new IllegalStateException());
+        trace.exception(status, new IllegalStateException());
+    }
+
+
+}
